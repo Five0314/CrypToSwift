9384d269476d3c4edabe71102e38d9daaff26b3c
董伍 <875169560@qq.com>
2020-04-01 09:39:22 +0800

    initialization

---
+++ CrypToSwift/CrypToSwift.h
@@ -0,0 +1,19 @@
+//
+//  CrypToSwift.h
+//  CrypToSwift
+//
+//  Created by 董伍 on 2019/3/13.
+//  Copyright © 2019 奇异的炸酱面. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrypToSwift.
+FOUNDATION_EXPORT double CrypToSwiftVersionNumber;
+
+//! Project version string for CrypToSwift.
+FOUNDATION_EXPORT const unsigned char CrypToSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrypToSwift/PublicHeader.h>
+
+
